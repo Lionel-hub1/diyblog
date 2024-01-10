1346c58e81625d963f6f9670162c364a7a5b41b3
lionel <ndabagajeanlionel@gmail.com>
2024-01-10 21:05:03 +0200

    Added Comment model and also docstrings for other models

--- contents/models.py
+++ contents/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Article(models.Model):
+    """Model for articles."""
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -10,16 +11,27 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='article_images/')
-    # Add any other fields an article should have
 
     def __str__(self):
         return self.title
 
 
 class Author(models.Model):
+    """Model for authors."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
     
-    
+
+class Comment(models.Model):
+    """Model for comments on articles."""
+    id = models.AutoField(primary_key=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
