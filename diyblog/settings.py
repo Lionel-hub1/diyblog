ce0e0b53794a0e46d1f413f59d2f44d3b3d297d5
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-03-08 17:23:06 +0200

    Update SECRET_KEY retrieval method

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rxtpm3d+6a*lpvwurp^rv%so3q5i*)wd0rdml)2_l0dlws9fz^"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
