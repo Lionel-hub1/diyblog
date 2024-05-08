0ab830979179469665e235b6cd9cfb7e61e2a254
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-05-08 13:49:47 +0200

    refactor User model to include email and password fields

--- contents/models.py
+++ contents/models.py
@@ -5,11 +5,13 @@ from django.db import models
 
 class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     profession = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     is_author = models.BooleanField(default=False)
+    password = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
