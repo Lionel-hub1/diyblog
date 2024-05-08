aa6f46736e0e8d90a16e41c00d8cadf641289f16
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2024-05-08 13:50:05 +0200

    Refactor UserSerializer and UserCreateSerializer in contents/serializers.py

--- contents/serializers.py
+++ contents/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer, UserSerializer as BaseUserSerializer
 from .models import *
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name',
+                  'profession', 'phone_number', 'is_author']
+
+
+class UserCreateSerializer(BaseUserRegistrationSerializer):
+    class Meta(BaseUserRegistrationSerializer.Meta):
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name',
+                  'profession', 'phone_number', 'is_author', 'password']
 
 
 class TypeSerializer(serializers.ModelSerializer):
