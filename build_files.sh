dca18748a19652b01c3d78ac62bd192a8dca2ef6
Jean Lionel Ndabaga <78383037+Lionel-hub1@users.noreply.github.com>
2026-04-15 12:10:57 +0200

    fix: Ensure superuser creation is attempted after collecting static files

--- build_files.sh
+++ build_files.sh
@@ -8,5 +8,7 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Collecting static files..."
-
 python3 manage.py collectstatic --noinput
+
+echo "Attempting to create superuser from environment variables..."
+python3 manage.py createsuperuser --noinput || true
